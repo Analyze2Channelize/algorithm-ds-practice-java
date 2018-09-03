c4a9590a7b68f4e4f2614a64531dc4ada50f9f00
Saurabh Saxena <analyze2channelize@gmail.com>
2018-09-03 21:49:12 +0530

    Algorithm and DS practice in Java

---
+++ AlgorithmPracticeJava/src/edu/saurabh/sorting/InsertionSort.java
@@ -0,0 +1,31 @@
+package edu.saurabh.sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+	public static void sortInPlace(Integer [] array) {
+		
+		int length = array.length;
+		for (int j =1 ; j< length;j++) {
+			Integer key = array[j];
+			int i = j-1;
+			while(i >= 0 && array[i] > key)
+			{
+				array[i+1] = array[i];
+				i = i-1;
+			}
+			array[i+1] = key;
+			
+		}
+		
+	}
+	
+	public static void main(String args[]) {
+		Integer [] unsorted = new Integer[] {10,23,4,8,6,1};
+		System.out.println(Arrays.toString(unsorted));
+		sortInPlace(unsorted);
+		System.out.println(Arrays.toString(unsorted));
+		
+	}
+}
